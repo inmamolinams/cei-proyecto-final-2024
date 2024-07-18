9de67b77e1f8efdce5a91aa236b9b51558d9af6d
inmamolinams <inmamolina94@hotmail.com>
2024-07-18 12:15:28 +0200

    cambio menu bocadillo

--- js/proyectos/case-study-dunas.js
+++ js/proyectos/case-study-dunas.js
@@ -4,14 +4,16 @@ const imagenes = document.querySelectorAll(".proyecto a");
 const modal = document.querySelector(".modal");
 const imgModal = document.querySelector(".modal img");
 
+// abrir la imagen
 imagenes.forEach(imagen => {
     imagen.addEventListener("click", evento => {
-        evento.preventDefault();
+        evento.preventDefault();   // Evita que el enlace navegue a la URL
             imgModal.setAttribute("src", imagen.getAttribute("href"));
             modal.classList.add("visible");
         });
 });
 
+// cerrar la imagen con un click en el fondo
 modal.addEventListener("click", () => {
     modal.classList.remove("visible");
 });
